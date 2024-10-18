a69bd034ddaa412739f601fe8122f48caefa680c
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-10-17 23:01:45 -0600

    Left disparity working

--- disparity_map.py
+++ disparity_map.py
@@ -17,11 +17,13 @@ class DisparityMap:
     def preprocessing(self, processing_type="GRAY"):
         if processing_type == "GRAY":
             self.left_processed = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2GRAY)
-            self.right_processed = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2GRAY)
+            self.right_processed = cv2.cvtColor(self.right_image, cv2.COLOR_BGR2GRAY)
         else:
             warnings.warn("Invalid preprocessing type, using BGR images instead")
             self.left_processed = self.left_image
             self.right_processed = self.right_image
+        self.left_processed = self.left_processed.astype("float32") / 255.0
+        self.right_processed = self.right_processed.astype("float32") / 255.0
 
     def estimate_disparity(self):
         image_size = np.shape(self.left_processed)
@@ -29,15 +31,13 @@ class DisparityMap:
         left_sad_image = np.zeros((image_size[0], image_size[1], 2))
         left_sad_image[:, :, :] = np.inf
         left_disparity = np.zeros((image_size[0], image_size[1]))
-        for disparity_level in range(1, self.disparity_range):  # niveles de disparidad
+        for disparity_level in range(0, self.disparity_range):  # niveles de disparidad
             shifted_image = np.roll(self.right_processed, disparity_level, axis=1)
-            shifted_image = np.abs(self.left_processed - shifted_image)
-            left_sad_image[:, :, 0] = signal.convolve2d(shifted_image, kernel_array, boundary='symm', mode='same')
-            temp_sad = np.argmin(left_sad_image, axis=0)
-            left_sad_image[:, :, 0] = np.min(left_sad_image, axis=0)
-            left_disparity[:, :, 1] = np.argmin(left_sad_image, axis=0)
-            ddppL[sadLEFT < sadLEFT_prev] = k
-            sadLEFT_prev[sadLEFT < sadLEFT_prev] = sadLEFT[sadLEFT < sadLEFT_prev]
+            left_sad_image[:, :, 1] = signal.convolve2d(np.abs(self.left_processed - shifted_image), kernel_array, boundary='symm', mode='same')
+            left_disparity[np.argmin(left_sad_image, axis=2) == 1] = disparity_level
+            left_sad_image[:, :, 0] = np.min(left_sad_image, axis=2)
+        return left_disparity
 
     def get_sparse_disparity(self, preprocess="GRAY"):
         self.preprocessing(preprocess)
+        return self.estimate_disparity()
