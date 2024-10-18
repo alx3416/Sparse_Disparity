94040ef4cf9536a90a62b84404698512df609fc9
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-10-17 18:33:20 -0600

    Update disparity_map.py

--- disparity_map.py
+++ disparity_map.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 import cv2
 import warnings
 
@@ -22,8 +23,19 @@ class DisparityMap:
             self.left_processed = self.left_image
             self.right_processed = self.right_image
 
+    def estimate_disparity(self):
+        image_size = np.shape(self.left_processed)
+        kernel_array = np.ones((self.block_size, self.block_size))
+        left_sad_image = np.zeros((image_size[0], image_size[1], 2))
+        left_sad_image[:, :, :] = np.inf
+        left_disparity = np.zeros((image_size[0], image_size[1]))
+        for disparity_level in range(1, self.disparity_range):  # niveles de disparidad
+            shifted_image = np.roll(self.right_processed, disparity_level, axis=1)
+            shifted_image = np.abs(self.left_processed - shifted_image)
+            left_sad_image[:, :, 0] = signal.convolve2d(shifted_image, kernel_array, boundary='symm', mode='same')
+            left_disparity[:, :, 1] = np.min(left_sad_image, axis=0)
+            ddppL[sadLEFT < sadLEFT_prev] = k
+            sadLEFT_prev[sadLEFT < sadLEFT_prev] = sadLEFT[sadLEFT < sadLEFT_prev]
+
     def get_sparse_disparity(self, preprocess="GRAY"):
         self.preprocessing(preprocess)
-
-
-
